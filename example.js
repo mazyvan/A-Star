dd189c30fdce448a29c28de013b5f4218349acc0
Iván Sánchez <mazyvan@hotmail.com>
2017-04-27 23:47:39 -0500

    primeras mejoras eddy

--- example.js
+++ example.js
@@ -1,24 +1,24 @@
-var HEIGHT = 40
-var WIDTH = 60
-var SIDE = 12
+var HEIGHT = 12
+var WIDTH = 30
+var SIDE = 35
 var world = document.getElementById('world')
-var extremes = {from: null, to: null}
+var extremes = { from: null, to: null }
 var dragging = false
 var worldMap = []
 var cells
 
 function drawSteps(path) {
-  if(path) {
-    for(var i = 0, l = path.length; i < l; i++) {
+  if (path) {
+    for (var i = 0, l = path.length; i < l; i++) {
       var step = path[i]
       cells[WIDTH * step.y + step.x].className = 'step'
     }
   }
 }
 
-var drawStepsAsync = eval(Wind.compile('async', function(path) {
-  if(path) {
-    for(var i = 0, l = path.length; i < l; i++) {
+var drawStepsAsync = eval(Wind.compile('async', function (path) {
+  if (path) {
+    for (var i = 0, l = path.length; i < l; i++) {
       var step = path[i]
       cells[WIDTH * step.y + step.x].className = 'step'
       $await(Wind.Async.sleep(10))
@@ -27,7 +27,7 @@ var drawStepsAsync = eval(Wind.compile('async', function(path) {
 }))
 
 function drawPath() {
-  Array.prototype.forEach.call(world.querySelectorAll('.step'), function(step) {
+  Array.prototype.forEach.call(world.querySelectorAll('.step'), function (step) {
     step.className = ''
   })
 
@@ -40,13 +40,13 @@ function drawPath() {
 
 function constructMap() {
   var fragment = document.createDocumentFragment()
-  for(var i = 0; i < HEIGHT; i++) {
+  for (var i = 0; i < HEIGHT; i++) {
     worldMap[i] = []
-    for(var j = 0; j < WIDTH; j++) {
+    for (var j = 0; j < WIDTH; j++) {
       var cell = document.createElement('span')
       cell.x = j
       cell.y = i
-      cell.style.height = cell.style.width = (SIDE - 1) + 'px'
+      cell.style.height = cell.style.width = (SIDE - 5) + 'px'
       cell.style.top = i * SIDE + 'px'
       cell.style.left = j * SIDE + 'px'
       worldMap[i][j] = { x: j, y: i, walkable: true }
@@ -54,42 +54,42 @@ function constructMap() {
     }
   }
   world.appendChild(fragment)
-  world.style.height = HEIGHT * SIDE - 1 + 'px'
-  world.style.width = WIDTH * SIDE - 1 + 'px'
+  world.style.height = HEIGHT * SIDE - 5 + 'px'
+  world.style.width = WIDTH * SIDE - 5 + 'px'
   cells = world.children
 }
 
 function inspectMouse() {
-  world.addEventListener('mousedown', function(e) {
-    if(e.button === 0 && e.target.tagName === 'SPAN') {
+  world.addEventListener('mousedown', function (e) {
+    if (e.button === 0 && e.target.tagName === 'SPAN') {
       dragging = true
     }
   }, false)
 
-  document.addEventListener('mouseup', function(e) {
-    if(e.button === 0) {
+  document.addEventListener('mouseup', function (e) {
+    if (e.button === 0) {
       dragging = false
     }
   }, false)
 
-  world.addEventListener('mousemove', function(e) {
+  world.addEventListener('mousemove', function (e) {
     var target = e.target
-    if(dragging && e.button === 0 && target.tagName === 'SPAN' && target.className !== 'from' && target.className !== 'to') {
+    if (dragging && e.button === 0 && target.tagName === 'SPAN' && target.className !== 'from' && target.className !== 'to') {
       target.className = 'obstacle'
       worldMap[target.y][target.x].walkable = false
     }
   }, false)
 
-  world.addEventListener('contextmenu', function(e) {
+  world.addEventListener('contextmenu', function (e) {
     e.preventDefault()
     var target = e.target
-    if(target.tagName === 'SPAN') {
+    if (target.tagName === 'SPAN') {
       worldMap[target.y][target.x].walkable = true
-      if(extremes.from === null) {
+      if (extremes.from === null) {
         target.className = 'from'
         extremes.from = target
       } else {
-        if(extremes.to === null) {
+        if (extremes.to === null) {
           target.className = 'to'
           extremes.to = target
         } else {
